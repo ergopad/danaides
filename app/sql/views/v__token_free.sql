cacdc53b8514fc549b1fca81457188e20a372c05
Viking Phoenix <vikingphoenixconsulting@gmail.com>
2022-10-01 16:16:47 -0600

    add group by address

--- app/sql/views/v__token_free.sql
+++ app/sql/views/v__token_free.sql
@@ -1,4 +1,4 @@
--- drop view v_tokens_free cascade
+-- drop view v_token_free cascade
 create or replace view v_token_free as
 	select address
 		, a.amount/power(10, t.decimals) as amount
@@ -6,4 +6,4 @@ create or replace view v_token_free as
 		join v_token_config tid on tid.token_id = a.token_id
 		join tokens t on t.token_id = a.token_id
 ;
--- drop table tokens_free; select * into tokens_free from v_tokens_free
+-- drop table token_free; select * into token_free from v_tokens_free
