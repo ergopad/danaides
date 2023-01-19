657c0323f954659886a0f0504a43d9bffe62ad23
Robert Pieter van Leeuwen <luivatra@gmail.com>
2023-01-19 15:03:21 +0100

    get box by id

---
+++ app/routes/utxo.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from utils.logger import logger, myself, Timer, LEIF
+from utils.db import eng
+from pydantic import BaseModel
+from typing import List
+from sqlalchemy import text
+from decimal import Decimal
+
+# mint
+# burn
+# get by id
+
+utxo_router = r = APIRouter()
+
+@r.get("/{box_id}")
+async def get_utxo_by_id(box_id: str):
+    sql = text(f'''
+        select id, box_id, ergo_tree, address, nergs, registers, hstore_to_json_loose(assets), transaction_id, creation_height, height
+        from utxos 
+        where box_id = :box_id
+    ''')
+    with eng.begin() as con:
+        res = con.execute(sql, {'box_id': box_id}).fetchone()
+
+    assets = []
+    for k in res["assets"]:
+        assets.append({"tokenId": k, "amount": res["assets"][k]})
+
+    return   {
+        "boxId": res["box_id"],
+        "value": res["nergs"],
+        "ergoTree": res["ergo_tree"],
+        "creationHeight": res["creation_height"],
+        "assets": assets,
+        "additionalRegisters": res["registers"],
+        "transactionId": res["transaction:_id"],
+        "index": 0
+    }
