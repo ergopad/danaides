820eae513a6858d5133ce2b9bed78baec3e54478
Robert Pieter van Leeuwen <luivatra@gmail.com>
2023-01-19 20:25:39 +0100

    getutxobyergotree

--- app/routes/utxo.py
+++ app/routes/utxo.py
@@ -12,6 +12,9 @@ from decimal import Decimal
 
 utxo_router = r = APIRouter()
 
+class ErgoTreeHex(BaseModel):
+    ergoTree: str
+
 @r.get("/{box_id}")
 async def get_utxo_by_id(box_id: str):
     sql = text(f'''
@@ -36,3 +39,34 @@ async def get_utxo_by_id(box_id: str):
         "transactionId": res["transaction_id"],
         "index": 0
     }
+
+@r.post("/byErgoTree")
+async def get_utxo_by_ergotree(ergoTree: ErgoTreeHex,  offset: int = 0, limit: int = 100):
+    sql = text(f'''
+        select id, box_id, ergo_tree, address, nergs, hstore_to_json_loose(registers) as registers, hstore_to_json_loose(assets) as assets, transaction_id, creation_height, height
+        from utxos 
+        where ergo_tree = :ergo_tree
+        order by id asc
+        offset :offset limit :limit
+    ''')
+    with eng.begin() as con:
+        res = con.execute(sql, {'ergo_tree': ergoTree.ergoTree, 'offset': offset, 'limit': limit}).fetchall()
+
+    result = []
+
+    for row in res:
+        assets = []
+        for k in row["assets"]:
+            assets.append({"tokenId": k, "amount": row["assets"][k]})
+        result.append({
+                "boxId": row["box_id"],
+                "value": row["nergs"],
+                "ergoTree": row["ergo_tree"],
+                "creationHeight": row["creation_height"],
+                "assets": assets,
+                "additionalRegisters": row["registers"],
+                "transactionId": row["transaction_id"],
+                "index": 0
+            })
+
+    return result
