8761ed018d5ba6c58e7f1a231bc0ffa6079d076d
Robert Pieter van Leeuwen <luivatra@gmail.com>
2023-05-22 10:28:12 +0200

    add aht staking

--- app/sql/views/d_token_status.sql
+++ app/sql/views/d_token_status.sql
@@ -17,12 +17,16 @@ create materialized view token_status as
 	-- neta
 	, neta_st as (select registers->'R4' as r4 from utxos where assets ? '9e5e5e0a3abbaeaf0e54e1e2ca4a6a96c9b7151dd6d7ddaf738be2f99a54dc2b' limit 1)
 	, neta_pl as (select registers->'R4' as r4 from utxos where assets ? '8a4f19b27efeaa328e9ba03f0752253713e6b477cdb9ea7d5e8857e671c48e60' limit 1)
+	-- aht
+	, aht_st as (select registers->'R4' as r4 from utxos where assets ? '4517aed531db665693495fd3544faf75717816509cb0987513d24939977abbb6' limit 1)
+	, aht_pl as (select registers->'R4' as r4 from utxos where assets ? '9d90e7567d066263104d1afd13bc23a1a259d733507bf1f33cd02155e7ff0df1' limit 1)
 	-- status
 		  select 'paideia' as token_name, paideia_st.r4 as str4, paideia_pl.r4 as plr4, 4 as dcml from paideia_st, paideia_pl
 	union select 'ergopad' as token_name, ergopad_st.r4 as str4, ergopad_pl.r4 as plr4, 2 from ergopad_st, ergopad_pl
 	union select 'egio' as token_name, egio_st.r4 as str4, egio_pl.r4 as plr4, 4 from egio_st, egio_pl
 	union select 'egiov2' as token_name, egiov2_st.r4 as str4, egiov2_pl.r4 as plr4, 4 from egiov2_st, egiov2_pl
 	union select 'neta' as token_name, neta_st.r4 as str4, neta_pl.r4 as plr4, 6 from neta_st, neta_pl
+	union select 'aht' as token_name, aht_st.r4 as str4, aht_pl.r4 as plr4, 6 from aht_st, aht_pl
 
     with no data;
 
